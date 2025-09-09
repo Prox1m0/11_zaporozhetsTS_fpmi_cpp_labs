03a67120030b72c2b68e7c71edc427901083ffe1
Taras <tarasztc@gmail.com>
2025-09-09 08:29:49 +0300

    Remade 5th task

--- semester_1/lab1_introduction/task5.cpp
+++ semester_1/lab1_introduction/task5.cpp
@@ -2,33 +2,27 @@
 //  task5.cpp
 //  labs
 //
-//  Created by Taras Zaporozhets on 4.09.25.
+//  Created by Taras Zaporozhets on 9.09.25.
 //
 #include <iostream>
-using namespace std;
-
-int gcd(int a, int b){
-    if (a == 0){
-        return b;
-    }
-    return gcd(b, a % b);
-}
-//второй способ
-int gcd_residual(int a, int b){
-    while (a != b){
-        if (a > b){
-            a -= b;
-        }
-        else{
-            b -= a;
-        }
-    }
-    return a;
-}
 
 int main(){
-    int n,m;
-    cout << "Введите n и m: ";
-    cin >> n >> m;
-    cout << gcd(n, m) << "\n";
+    int n, m, min, max;
+    
+    std::cout << "Введите n и m: ";
+    std::cin >> n >> m;
+    
+    if (n < m){
+        min = n;
+        max = m;
+    }
+    else{
+        min = m;
+        max = n;
+    }
+    for (int i = 1; i <= min; i++){
+        if ((min % i == 0) && (max % i == 0)){
+            std::cout << i << '\n';
+        }
+    }
 }
