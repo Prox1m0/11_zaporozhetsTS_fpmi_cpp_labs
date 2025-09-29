8b63dc2da4b18c7931efd2de1d69c31fc03d370f
Taras <tarasztc@gmail.com>
2025-09-29 15:57:01 +0300

    3 lab solved

--- semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_dynamic.cpp
@@ -1,7 +1,148 @@
 
 // solve task with usage of
 // dymanic arrays
-int main() {
+//
+//  task27_dynamic.cpp
+//  labs
+//
+//  Created by Taras Zaporozhets on 29.09.25.
+//
 
+#include <iostream>
+#include <random>
+
+//функция заполнения массива рандомными числами
+void random(int* random_arr, int size){
+    
+    int lower_limit, upper_limit;
+    
+    std::cout << "Enter the range of random: ";
+    
+    if (!(std::cin >> lower_limit >> upper_limit) || lower_limit > upper_limit){
+        std::cout << "Incorrect input!\n";
+        
+        delete [] random_arr;
+        std::exit(1);
+    }
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::uniform_int_distribution<> distrib(lower_limit, upper_limit);
+    
+    for (int i = 0; i < size; i++){
+        random_arr[i] = distrib(gen);
+    }
+    
+}
+
+//функция заполнения массива вручную
+void hand(int* hand_arr, int size){
+    
+    std::cout << "Enter the integer elements of array: ";
+    
+    for (int i = 0; i < size; i++){
+        if (!(std::cin >> hand_arr[i])){
+            std::cout << "Incorrect Input!\n";
+            
+            delete [] hand_arr;
+            std::exit(1);
+        }
+    }
+    
+}
+
+void transform(int* new_arr, int size){
+    
+    int start = 0;
+    for (int i = 0; i < size; i++){
+        if (new_arr[i] % 2 == 0){
+            if (start != i){
+                 
+                int temp = new_arr[i];
+                
+                for (int j = i; j > start; j--){
+                    new_arr[j] = new_arr[j-1];
+                }
+                new_arr[start] = temp;
+            }
+            start++;
+        }
+    }
+}
+
+int main(){
+    
+    int n = 0, sum = 0;
+    char option;
+    
+    std::cout << "Enter the size of array: ";
+    if(!(std::cin >> n) || n <= 0){
+        std::cout << "Incorrect Input!\n";
+        std::exit(1);
+    }
+    
+    int* arr = new int [n];
+    
+    std::cout << "Choose Hand Input or Random Input (h/r): ";
+    std::cin >> option;
+    
+    //обработка выбора ввода
+    switch (option) {
+        case 'h':
+            hand(arr, n);
+            break;
+        case 'r':
+            random(arr, n);
+            break;
+        default:
+            std::cout << "Incorrect option input!\n";
+            
+            delete [] arr;
+            std::exit(1);
+    }
+    
+    //задание 1 с минимальным по модулю элементом
+    int min = std::abs(arr[0]);
+    int value = arr[0];
+    
+    for (int i = 1; i < n; i++){
+        if (std::abs(arr[i]) < min){
+            min = std::abs(arr[i]);
+            value = arr[i];
+        }
+    }
+    
+    std::cout << "Element with minimal absolute value: " << value  << " (abs = " << min << ")\n";
+    
+    //задание 2 с суммой всех элементов после последнего нулевого
+    for (int i = 0; i < n; i++){
+        sum += arr[i];
+        if (arr[i] == 0){
+            sum = 0;
+        }
+    }
+    std::cout << "Sum of all elements after the last 0:  " << sum;
+    
+    //преобразование массива
+    std::cout << "\nArray before transformation: ";
+    
+    for (int i = 0; i < n; i++){
+        std::cout << arr[i] << " ";
+    }
+    
+    transform(arr, n);
+    
+    std::cout << "\nArray after trnsformation: ";
+    
+    for (int i = 0; i < n; i++){
+        std::cout << arr[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
+    delete []  arr;
+    
     return 0;
 }
+
--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -1,7 +1,135 @@
 
 // solve task with usage of
 // static arrays
-int main() {
+//
+//  task9_static.cpp
+//  labs
+//
+//  Created by Taras Zaporozhets on 25.09.25.
+//
 
+#include <iostream>
+#include <random>
+
+//функция заполнения массива рандомными числами
+void random(double random_arr[], int size){
+    
+    int lower_limit, upper_limit;
+    
+    std::cout << "Enter the range of random: ";
+    
+    if (!(std::cin >> lower_limit >> upper_limit) || lower_limit > upper_limit){
+        std::cout << "Incorrect input!\n";
+        std::exit(1);
+    }
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::uniform_real_distribution<> distrib(lower_limit, upper_limit);
+    
+    for (int i = 0; i < size; i++){
+        random_arr[i] = distrib(gen);
+    }
+    
+}
+
+//функция заполнения массива вручную
+void hand(double hand_arr[], int size){
+    
+    std::cout << "Enter the elements of array: ";
+    
+    for (int i = 0; i < size; i++){
+        if (!(std::cin >> hand_arr[i])){
+            std::cout << "Incorrect Input!\n";
+            std::exit(1);
+        }
+    }
+    
+}
+
+void transform(double new_arr[], int size, int P){
+    
+    int start = 0;
+    for (int i = 0; i < size; i++) {
+        if (new_arr[i] < P) {
+            double temp = new_arr[i];
+            for (int j = i; j > start; j--) {
+                new_arr[j] = new_arr[j-1];
+            }
+            new_arr[start] = temp;
+            start++;
+        }
+    }
+}
+
+int main(){
+    double arr[100], p;
+    int n, count = 0, sum = 0;
+    char option;
+    
+    std::cout << "Enter size of array <= 100: ";
+    if (!(std::cin >> n) || n <= 0 || n > 100){
+        std::cout << "Incorrect input!\n";
+        std::exit(1);
+    }
+    
+    std::cout << "Choose Hand Input or Random Input (h/r): ";
+    std::cin >> option;
+    
+    //обработка выбора ввода
+    switch (option) {
+        case 'h':
+            hand(arr, n);
+            break;
+        case 'r':
+            random(arr, n);
+            break;
+        default:
+            std::cout << "Incorrect option input!\n";
+            std::exit(1);
+    }
+    
+    //задание 1 (сравнение с P)
+    std::cout << "Enter the P: ";
+    if (!(std::cin >> p)){
+        std::cout << "Incorrect Input!";
+        std::exit(1);
+    }
+    
+    std::cout << "Number of elements that are less than P: ";
+    for (int i = 0; i < n; i++){
+        if (arr[i] < p){
+            count++;
+        }
+    }
+    
+    std::cout << count << "\n";
+    
+    //задание 2 (сумма целых частей)
+    //считается, что при отсутствии отрицательных чисел берется сумма целых частей всех элементов массива, так как в условии про обработку такого случая ничего не сказано
+    for (int i = 0; i < n; i++){
+        sum += static_cast<int>(arr[i]);
+        if (arr[i] < 0){
+            sum = 0;
+        }
+    }
+    std::cout << "Integer sum of all elemnets after the last negative: " << sum << "\n";
+    
+    //преобразовние массива
+    std::cout << "Array before tranformation:\n";
+    for (int i = 0; i < n; i++){
+        std::cout << arr[i] << " ";
+    }
+    
+    transform(arr, n, p);
+    
+    std::cout << "\nArray after tranformation:\n";
+    for (int i = 0; i < n; i++){
+        std::cout << arr[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
     return 0;
 }
